276ba625802dd4a7e16b30f085c405f91efff373
Andriy Uvarov <andriy.uvarov@windmill.ch>
2022-09-04 12:37:51 +0300

    Second part

--- react-base-fast/src/App.tsx
+++ react-base-fast/src/App.tsx
@@ -1,51 +1,72 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ITodo } from "./interfaces/todos";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
-import AddTodo from "./Todo/AddTodo";
+// import AddTodo from "./Todo/AddTodo";
+import Loader from "./Loader";
+import Modal from "./Modal/Modal";
+
+const AddTodo = React.lazy(() => new Promise<any>(resolve => {
+  setTimeout(() => {
+    resolve(import('./Todo/AddTodo'));
+  }, 3000)
+}));
 
 function App(): JSX.Element {
-  const [todos, setTodos] = React.useState<ITodo[]>([
-    { id: 1, comleted: false, title: 'Buy the bread' },
-    { id: 2, comleted: true, title: 'Buy the mob' },
-    { id: 3, comleted: false, title: 'Buy the img' },
-  ]);
+  const [todos, setTodos] = React.useState<ITodo[]>([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
-function toggleTodo(id: number): void {
-  const newTodos = todos.map((todo: ITodo) => {
-    if (todo.id === id) {
-      todo.comleted = !todo.comleted
-    }
-    return todo;
+useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+  .then(response => response.json())
+  .then((todoss) => {
+    setTimeout(() => {
+      setTodos(todoss);
+      setIsLoading(false);
+    }, 2000)
   })
+}, []);
 
-  setTodos(newTodos)
-}
-
-function removeTodo(id: number): void {
-  setTodos(todos.filter((todo) => todo.id !== id));
-}
-
-function addTodo(title: string): void {
-  setTodos(
-    todos.concat([
-      {
-        id: Date.now(),
-        title: title,
-        comleted: false,
+  function toggleTodo(id: number): void {
+    const newTodos = todos.map((todo: ITodo) => {
+      if (todo.id === id) {
+        todo.comleted = !todo.comleted
       }
-    ])
-  );
-}
+      return todo;
+    })
+
+    setTodos(newTodos)
+  }
+
+  function removeTodo(id: number): void {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function addTodo(title: string): void {
+    setTodos(
+      todos.concat([
+        {
+          id: Date.now(),
+          title: title,
+          comleted: false,
+        }
+      ])
+    );
+  }
 
   return (
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>Test  react </h1>
 
-        <AddTodo onCreate={addTodo}></AddTodo>
+        <Modal></Modal>
 
-        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}></TodoList> : <p>Empty</p>}
+        <React.Suspense fallback={<p>Loading TODOs...</p>}>
+          <AddTodo onCreate={addTodo}></AddTodo>
+        </React.Suspense>
+
+        {isLoading && <Loader></Loader>}
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}></TodoList> : !isLoading && <p>Empty</p>}
       </div>
     </Context.Provider>
   );
--- react-base-fast/src/Todo/AddTodo.tsx
+++ react-base-fast/src/Todo/AddTodo.tsx
@@ -4,29 +4,48 @@ interface IAddTodoProps {
   onCreate(value: string): void
 }
 
+interface IUseInputValue {
+  bind: {value: string, onChange(e: React.ChangeEvent<HTMLInputElement>): void},
+  clear: () => void,
+  value: () => string,
+}
+
+function useInputValue(defaultValue = ''): IUseInputValue {
+  const [value, setValue] = useState(defaultValue);
+
+  return {
+    bind: {
+      value,
+      onChange: (event: React.ChangeEvent<HTMLInputElement>) => setValue(event.target.value),
+    },
+    clear: () => setValue(''),
+    value: () => value,
+  }
+}
+
 function AddTodo({ onCreate }: IAddTodoProps): JSX.Element {
+  const input = useInputValue('');
+
   const styles = {
     form: {
       marginBottom: '1rem',
     }
   }
 
-  const [value, setValue] = useState('');
-
   const submitHandler = (e: React.SyntheticEvent): void => {
     e.preventDefault();
 
-    if (value.trim()) {
-      onCreate(value);
-      setValue('');
+    if (input.value().trim()) {
+      onCreate(input.value());
+      input.clear();
     }
   }
 
-  console.log(value);
+  console.log(input.value);
 
   return (
     <form style={styles.form} onSubmit={(e) => submitHandler(e)}>
-      <input value={value} onChange={(e) => setValue(e.target.value)}></input>
+      <input {...input.bind}></input>
       <button type='submit'>Add todo</button>
     </form>
   );
