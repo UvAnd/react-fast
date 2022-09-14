ff3af92eb72008e81cd09a62696bb59b9af7b8b6
Andriy Uvarov <andriy.uvarov@windmill.ch>
2022-09-14 11:04:53 +0300

    Second part

--- todo-react-app/src/components/app/app.tsx
+++ todo-react-app/src/components/app/app.tsx
@@ -4,14 +4,30 @@ import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
+import { useState } from 'react';
 
 const App = (): JSX.Element => {
 
-  const todoData = [
+  const todoDataBase = [
     { label: 'Drink Coffee', important: false, id: 1 },
     { label: 'Make Awesome App', important: true, id: 2 },
     { label: 'Have a lunch', important: false, id: 3 }
   ];
+
+  const [todoData, setTodoData] = useState([...todoDataBase]);
+
+  const deleteTodo = (id: number): void => {
+    // const idx = todoData.findIndex((el) => el.id === id);
+    // const newArray = [
+    //   ...todoData.slice(0, idx),
+    //   ...todoData.slice(idx + 1)
+    // ]
+
+    // Check variant with (prev) => {prev.filter((item) => item.id !== id}
+
+    const newArray = todoData.filter((item) => item.id !== id);
+    setTodoData(newArray);
+  }
 
   return (
     <div className="todo-app">
@@ -21,7 +37,7 @@ const App = (): JSX.Element => {
         <ItemStatusFilter />
       </div>
 
-      <TodoList todos={todoData} />
+      <TodoList todos={todoData} onDeleted={deleteTodo} />
     </div>
   );
 };
--- todo-react-app/src/components/todo-list-item/todo-list-item.tsx
+++ todo-react-app/src/components/todo-list-item/todo-list-item.tsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }: any): JSX.Element => {
+const TodoListItem = ({ label, important = false, onDeleted }: any): JSX.Element => {
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
+  const [isTodoDone, setIsTodoDone] = useState(false);
+  const [isTodoImportant, setIsTodoImportant] = useState(important);
+
+  const onLabelClick = (): void => {
+    setIsTodoDone((prev) => !prev);
+  }
+
+  const onImportantClick = (): void => {
+    setIsTodoImportant((prev: any) => !prev);
+  }
+
+  const doneClass = isTodoDone ? 'done' : '';
+  const importantClass = isTodoImportant ? 'important' : '';
+
+  const classNames = `todo-list-item ${doneClass} ${importantClass}`;
+
+console.log(classNames);
 
   return (
-    <span className="todo-list-item">
+    <span className={classNames}>
       <span
         className="todo-list-item-label"
-        style={style}>
+        onClick={onLabelClick}
+      >
         {label}
       </span>
 
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
+      <button
+        type="button"
+        className="btn btn-outline-success btn-sm float-right"
+        onClick={onImportantClick}
+      >
         <i className="fa fa-exclamation" />
       </button>
 
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
+      <button
+        type="button"
+        className="btn btn-outline-danger btn-sm float-right"
+        onClick={onDeleted}
+      >
         <i className="fa-regular fa-trash"></i>
       </button>
     </span>
